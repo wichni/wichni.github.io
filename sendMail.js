eb7086a4307698c2d8d80e6adabe0c262aef0885
jakub <jakub.wichniarek@mayeryn.com>
2021-02-23 10:42:43 +0100

    adding new cv

--- sendMail.js
+++
@@ -1,6 +0,0 @@
-function sendEmail() {
-    var link = 'mailto:wichniarekjakub89@gmail.com?subject=Message from ' +
-        document.getElementById('email').value +
-        '&body=' + document.getElementById('message').value;
-    window.location.href = link;
-}
